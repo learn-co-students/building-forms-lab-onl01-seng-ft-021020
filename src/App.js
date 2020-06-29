d5451d1248ade9eb0a764a8a2facf48fa11e109f
github username <github email address>
2020-06-28 19:58:17 -0700

    Done.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
+
 import React, { Component } from 'react';
-import BandsContainer from './containers/BandsContainer';
+import BandsContainer from './containers/BandsContainer'
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <BandsContainer />
-      </div>
-    );
-  }
+render() {
+  return (
+    <div className="App">
+      <BandsContainer />
+    </div>
+  );
+}
 };
 
 export default App;
